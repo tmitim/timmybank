6c18a28799aac30877ede865d1e467ee8590cd9a
tmitim <igetemail@gmail.com>
2016-10-22 20:13:58 -0700

    Cors stuff

--- src/main/java/com/tmitim/timmybank/BasicApplication.java
+++ src/main/java/com/tmitim/timmybank/BasicApplication.java
@@ -1,5 +1,11 @@
 package com.tmitim.timmybank;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
 
 import io.dropwizard.Application;
@@ -32,6 +38,17 @@ public class BasicApplication extends Application<BasicConfiguration> {
 
 	@Override
 	public void run(BasicConfiguration configuration, Environment environment) {
+		// Enable CORS headers
+		final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
 		final DBIFactory factory = new DBIFactory();
 		final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "mysql");
 
