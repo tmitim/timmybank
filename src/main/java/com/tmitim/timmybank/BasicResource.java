9df585c3d757412ca0a34c0fce671b2f92f20354
tmitim <igetemail@gmail.com>
2016-10-23 01:10:55 -0700

    Connecting to modo

--- src/main/java/com/tmitim/timmybank/BasicResource.java
+++ src/main/java/com/tmitim/timmybank/BasicResource.java
@@ -14,8 +14,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import com.auth0.jwt.JWTSigner;
+
 @Path("/api/")
 public class BasicResource {
+
+	private String URL_BASE = "https://hack.modoapi.com/1.0.0-dev/";
 
 	private TaskDao taskDao;
 
@@ -29,6 +40,8 @@ public class BasicResource {
 	public Response getBase() {
 		Map<String, String> map = new HashMap<>();
 		map.put("hello", "world");
+
+		post("https://hack.modoapi.com/1.0.0-dev/vault/get_type_list");
 		return Response.ok(map).build();
 	}
 
@@ -76,5 +89,70 @@ public class BasicResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAccountableTasks(@PathParam("accountableId") int accountableId) {
 		return Response.ok(taskDao.getAccountableTasks(accountableId)).build();
+	}
+
+	private String getJwt() {
+		final String secret = "";
+
+		final long iat = System.currentTimeMillis() / 1000L; // issued at claim
+		final long exp = iat + 60L; // expires claim. In this case the token
+									// expires in 60 seconds
+
+		final JWTSigner signer = new JWTSigner(secret);
+		final HashMap<String, Object> claims = new HashMap<String, Object>();
+		claims.put("api_key", "97964300-57d5-4585-8624-d2ddd06b0500");
+		claims.put("iat", iat);
+
+		final String jwt = signer.sign(claims);
+
+		return jwt;
+	}
+
+	private String post(String url) {
+
+		HttpClient httpClient = HttpClientBuilder.create().build(); // Use this
+																	// instead
+
+		String json = "";
+		try {
+			HttpPost request = new HttpPost(url);
+
+			request.addHeader("Authorization", "Token " + getJwt());
+
+			System.out.println(getJwt());
+			HttpResponse response = httpClient.execute(request);
+
+			json = EntityUtils.toString(response.getEntity());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return json;
+	}
+
+	private String post(String url, String jsonBody) {
+
+		HttpClient httpClient = HttpClientBuilder.create().build(); // Use this
+																	// instead
+
+		String json = "";
+		try {
+			HttpPost request = new HttpPost(url);
+
+			request.addHeader("Authorization", "Token " + getJwt());
+			StringEntity params = new StringEntity(jsonBody);
+			// request.addHeader("content-type",
+			// "application/x-www-form-urlencoded");
+			request.setEntity(params);
+			HttpResponse response = httpClient.execute(request);
+
+			json = EntityUtils.toString(response.getEntity());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return json;
 	}
 }
